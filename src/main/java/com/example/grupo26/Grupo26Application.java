f0968aca4d663444ba5b44fc16c5ad532426c65b
lucianomp9 <lucianoperez12@gmail.com>
2024-06-14 19:27:21 -0300

    Add src project folder

---
+++ src/main/java/com/example/grupo26/Grupo26Application.java
@@ -0,0 +1,13 @@
+package com.example.grupo26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo26Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo26Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/grupo26/Grupo26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupo26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
